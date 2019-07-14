f0de5ff127b28ae608c14dc2920a61970889c468
Khare, Saurabh <saurabh.khare@rakuten.com>
2019-07-14 19:39:55 +0900

    HomeScreen menu added.

--- app/src/main/java/com/axion/news/views/activity/MainActivity.kt
+++ app/src/main/java/com/axion/news/views/activity/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.axion.news.views.activity
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.axion.news.R
 import com.axion.news.databinding.ActivityMainBinding
 import dagger.android.DispatchingAndroidInjector
@@ -19,10 +26,20 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // Set up ActionBar
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+
+        NavigationUI.setupActionBarWithNavController(this, Navigation.findNavController(this, R.id.home_navigation_fragment))
         //TODO: Add menu as profile
 
         //TODO: add bottom navigation bar
     }
 
     override fun supportFragmentInjector() = dispatchingAndroidInjector
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 }
