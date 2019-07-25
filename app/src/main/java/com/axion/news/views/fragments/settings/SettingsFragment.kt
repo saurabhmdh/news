0b564f27c6a0531cc2d49b51d693f4d9d77a7fdf
Khare, Saurabh <saurabh.khare@rakuten.com>
2019-07-25 16:39:57 +0900

    bottom navigation bar color has been changed

--- app/src/main/java/com/axion/news/views/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/axion/news/views/fragments/settings/SettingsFragment.kt
@@ -1,7 +1,15 @@
 package com.axion.news.views.fragments.settings
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.axion.news.di.Injectable
 
 class SettingsFragment: Fragment(), Injectable {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
